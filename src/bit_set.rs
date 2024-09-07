9f5c856b011da982d8ac605df4bd7b7156220952
Banyc <36535895+Banyc@users.noreply.github.com>
2024-09-07 17:50:41 +0800

    feat: `BitSet::len()`

--- src/bit_set.rs
+++ src/bit_set.rs
@@ -1,22 +1,36 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BitSet {
     integers: Vec<usize>,
+    count: usize,
 }
 impl BitSet {
+    #[must_use]
     pub fn new(bits: usize) -> Self {
         let bytes = bits.div_ceil(bits);
         let integers = bytes.div_ceil(core::mem::size_of::<usize>());
         Self {
             integers: vec![0; integers],
+            count: 0,
         }
     }
+    #[must_use]
     pub fn capacity(&self) -> usize {
         self.integers.len() * core::mem::size_of::<usize>()
+    }
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.count
+    }
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
     }
 
     pub fn clear_all(&mut self) {
         self.integers.iter_mut().for_each(|x| *x = 0);
+        self.count = 0;
     }
+    #[must_use]
     pub fn get(&self, index: usize) -> bool {
         let integer = self.integers[integer_index(index)];
         let pos = 1 << bit_offset(index);
@@ -25,8 +39,15 @@ impl BitSet {
     }
     fn bit_op(&mut self, bit_index: usize, op: impl Fn(usize, usize) -> usize) {
         let integer = &mut self.integers[integer_index(bit_index)];
+        let prev = integer.count_ones();
         let pos = 1 << bit_offset(bit_index);
         *integer = op(*integer, pos);
+        let curr = integer.count_ones();
+        match prev.cmp(&curr) {
+            std::cmp::Ordering::Less => self.count += usize::try_from(curr - prev).unwrap(),
+            std::cmp::Ordering::Equal => (),
+            std::cmp::Ordering::Greater => self.count -= usize::try_from(prev - curr).unwrap(),
+        }
     }
     pub fn set(&mut self, index: usize) {
         self.bit_op(index, |integer, pos| integer | pos);
@@ -52,9 +73,11 @@ mod tests {
 
     #[test]
     fn test_bit_set() {
-        let mut ba = BitSet::new(16);
-        assert!(!ba.get(1));
-        ba.set(1);
-        assert!(ba.get(1));
+        let mut b = BitSet::new(16);
+        assert!(!b.get(1));
+        assert!(b.is_empty());
+        b.set(1);
+        assert!(b.get(1));
+        assert_eq!(b.len(), 1);
     }
 }
