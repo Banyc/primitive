a944197b66830b43538c9f6e870b9b7d7a441bf2
Banyc <36535895+Banyc@users.noreply.github.com>
2024-09-07 16:42:19 +0800

    feat: `Queue`, `EmptyBorrowVec`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#![cfg_attr(feature = "nightly", feature(test))]
+#[cfg(feature = "nightly")]
+extern crate test;
+
 pub mod indexed_queue;
 pub mod notify;
+pub mod priority;
+pub mod static_borrow_vec;
 pub mod vec_seg;
