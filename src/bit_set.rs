a267eaac4711eb4f9123d0dc1d05d1502a33c68f
Banyc <36535895+Banyc@users.noreply.github.com>
2024-09-09 16:14:27 +0800

    chore: rename `BitSet::words`

--- src/bit_set.rs
+++ src/bit_set.rs
@@ -2,37 +2,38 @@ use crate::{Clear, Len};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct BitSet {
-    integers: Vec<usize>,
+    words: Vec<usize>,
     count: usize,
 }
 impl BitSet {
     #[must_use]
     pub fn new(bits: usize) -> Self {
         let bytes = bits.div_ceil(bits);
-        let integers = bytes.div_ceil(core::mem::size_of::<usize>());
+        let words = bytes.div_ceil(core::mem::size_of::<usize>());
         Self {
-            integers: vec![0; integers],
+            words: vec![0; words],
             count: 0,
         }
     }
     #[must_use]
     pub fn capacity(&self) -> usize {
-        self.integers.len() * core::mem::size_of::<usize>()
+        self.words.len() * core::mem::size_of::<usize>()
     }
 
     #[must_use]
     pub fn get(&self, index: usize) -> bool {
-        let integer = self.integers[integer_index(index)];
+        let word = self.words[word_index(index)];
         let pos = 1 << bit_offset(index);
-        let is_empty = integer & pos == 0;
+        let is_empty = word & pos == 0;
         !is_empty
     }
-    fn bit_op(&mut self, bit_index: usize, op: impl Fn(usize, usize) -> usize) {
-        let integer = &mut self.integers[integer_index(bit_index)];
-        let prev = integer.count_ones();
+    fn bit_op(&mut self, bit_index: usize, op: impl Fn(BitOpArgs) -> usize) {
+        let word = &mut self.words[word_index(bit_index)];
+        let prev = word.count_ones();
         let pos = 1 << bit_offset(bit_index);
-        *integer = op(*integer, pos);
-        let curr = integer.count_ones();
+        let args = BitOpArgs { word: *word, pos };
+        *word = op(args);
+        let curr = word.count_ones();
         match prev.cmp(&curr) {
             std::cmp::Ordering::Less => self.count += usize::try_from(curr - prev).unwrap(),
             std::cmp::Ordering::Equal => (),
@@ -40,13 +41,13 @@ impl BitSet {
         }
     }
     pub fn set(&mut self, index: usize) {
-        self.bit_op(index, |integer, pos| integer | pos);
+        self.bit_op(index, |args| args.word | args.pos);
     }
     pub fn clear_bit(&mut self, index: usize) {
-        self.bit_op(index, |integer, pos| integer & !pos);
+        self.bit_op(index, |args| args.word & !args.pos);
     }
     pub fn toggle(&mut self, index: usize) {
-        self.bit_op(index, |integer, pos| integer ^ pos);
+        self.bit_op(index, |args| args.word ^ args.pos);
     }
 }
 impl Len for BitSet {
@@ -56,14 +57,20 @@ impl Len for BitSet {
 }
 impl Clear for BitSet {
     fn clear(&mut self) {
-        self.integers.iter_mut().for_each(|x| *x = 0);
+        self.words.iter_mut().for_each(|x| *x = 0);
         self.count = 0;
     }
 }
+struct BitOpArgs {
+    pub word: usize,
+    pub pos: usize,
+}
 
-fn integer_index(bit_index: usize) -> usize {
+#[must_use]
+fn word_index(bit_index: usize) -> usize {
     bit_index / core::mem::size_of::<usize>()
 }
+#[must_use]
 fn bit_offset(bit_index: usize) -> usize {
     bit_index % core::mem::size_of::<usize>()
 }
