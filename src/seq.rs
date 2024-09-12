d0180812bcabdbce218d2a0da8e850cae20774bc
Banyc <36535895+Banyc@users.noreply.github.com>
2024-09-13 02:58:30 +0800

    feat: slice `Seq`

--- src/seq.rs
+++ src/seq.rs
@@ -1,5 +1,7 @@
 pub trait Seq<T> {
     fn as_slice(&self) -> &[T];
+}
+pub trait SeqMut<T>: Seq<T> {
     fn as_slice_mut(&mut self) -> &mut [T];
 }
 
@@ -7,14 +9,35 @@ impl<T> Seq<T> for Vec<T> {
     fn as_slice(&self) -> &[T] {
         self
     }
+}
+impl<T> SeqMut<T> for Vec<T> {
     fn as_slice_mut(&mut self) -> &mut [T] {
         self
     }
 }
+
 impl<T, const N: usize> Seq<T> for [T; N] {
     fn as_slice(&self) -> &[T] {
         self
     }
+}
+impl<T, const N: usize> SeqMut<T> for [T; N] {
+    fn as_slice_mut(&mut self) -> &mut [T] {
+        self
+    }
+}
+
+impl<T> Seq<T> for &[T] {
+    fn as_slice(&self) -> &[T] {
+        self
+    }
+}
+impl<T> Seq<T> for &mut [T] {
+    fn as_slice(&self) -> &[T] {
+        self
+    }
+}
+impl<T> SeqMut<T> for &mut [T] {
     fn as_slice_mut(&mut self) -> &mut [T] {
         self
     }
@@ -28,6 +51,6 @@ mod tests {
     fn test_seq() {
         let mut v = vec![1, 2];
         assert_eq!(Seq::as_slice(&v)[0], 1);
-        assert_eq!(Seq::as_slice_mut(&mut v)[0], 1);
+        assert_eq!(SeqMut::as_slice_mut(&mut v)[0], 1);
     }
 }
