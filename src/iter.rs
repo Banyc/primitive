73f925ec8e61c1c4171ce10df84e91341e4480be
Banyc <36535895+Banyc@users.noreply.github.com>
2024-09-26 14:20:45 +0800

    feat: `Lookahead1Mut`

--- src/iter.rs
+++ src/iter.rs
@@ -1,3 +1,5 @@
+use std::cell::RefCell;
+
 /// # Example
 ///
 /// ```rust
@@ -65,5 +67,53 @@ where
     pub fn pop(&mut self) -> Option<T> {
         let next = self.iter.next();
         core::mem::replace(&mut self.next, next)
+    }
+}
+
+#[derive(Debug)]
+pub struct Lookahead1Mut<'a, I, T> {
+    iter: I,
+    next: RefCell<Option<&'a mut T>>,
+}
+impl<'a, I, T> Lookahead1Mut<'a, I, T>
+where
+    I: Iterator<Item = &'a mut T>,
+{
+    #[must_use]
+    pub fn new(mut iter: I) -> Self {
+        let next = iter.next();
+        let next = RefCell::new(next);
+        Self { iter, next }
+    }
+
+    #[must_use]
+    pub fn peek(&self) -> &RefCell<Option<&'a mut T>> {
+        &self.next
+    }
+    pub fn pop(&mut self) -> Option<&'a mut T> {
+        let next = self.iter.next();
+        core::mem::replace(&mut self.next.borrow_mut(), next)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lookahead1_mut() {
+        let mut vec = vec![1, 2, 3];
+        let mut iter = Lookahead1Mut::new(vec.iter_mut());
+        loop {
+            {
+                let mut int = iter.peek().borrow_mut();
+                let Some(int) = int.as_deref_mut() else {
+                    break;
+                };
+                *int = 0;
+            }
+            iter.pop();
+        }
+        assert_eq!(vec, [0, 0, 0]);
     }
 }
