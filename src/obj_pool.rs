cf0934cfc74b21ec2bbb71b368bddc522f09c346
Banyc <36535895+Banyc@users.noreply.github.com>
2024-10-27 00:11:36 +0800

    feat: `ArcObjectPool::take_scoped()`

--- src/obj_pool.rs
+++ src/obj_pool.rs
@@ -1,4 +1,8 @@
-use core::num::NonZeroUsize;
+use core::{
+    mem::MaybeUninit,
+    num::NonZeroUsize,
+    ops::{Deref, DerefMut},
+};
 use std::sync::Arc;
 
 use crate::{ops::ring::RingSpace, sync::mutex::SpinMutex, Capacity, Len};
@@ -100,6 +104,10 @@ impl<T> ArcObjectPool<T> {
             .unwrap_or_else(|| (self.alloc)())
     }
     #[must_use]
+    pub fn take_scoped(&mut self) -> ObjectScoped<T> {
+        ObjectScoped::new(self.recycler(), self.take())
+    }
+    #[must_use]
     pub fn recycler(&self) -> ObjectRecycler<T> {
         ObjectRecycler {
             stacks: Arc::clone(&self.stacks),
@@ -134,6 +142,38 @@ impl<T> Clone for ObjectRecycler<T> {
     }
 }
 
+#[derive(Debug)]
+pub struct ObjectScoped<T> {
+    recycler: ObjectRecycler<T>,
+    obj: MaybeUninit<T>,
+}
+impl<T> ObjectScoped<T> {
+    pub fn new(recycler: ObjectRecycler<T>, obj: T) -> Self {
+        Self {
+            recycler,
+            obj: MaybeUninit::new(obj),
+        }
+    }
+}
+impl<T> Deref for ObjectScoped<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { self.obj.assume_init_ref() }
+    }
+}
+impl<T> DerefMut for ObjectScoped<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { self.obj.assume_init_mut() }
+    }
+}
+impl<T> Drop for ObjectScoped<T> {
+    fn drop(&mut self) {
+        let obj = core::mem::replace(&mut self.obj, MaybeUninit::uninit());
+        let obj = unsafe { obj.assume_init() };
+        self.recycler.put(obj);
+    }
+}
+
 #[cfg(feature = "nightly")]
 #[cfg(test)]
 mod benches {
@@ -145,6 +185,42 @@ mod benches {
     #[derive(Default)]
     struct Data {
         _buf: [u8; DATA_SIZE],
+    }
+
+    #[bench]
+    #[ignore]
+    fn bench_lockfree_object_pool_owned(bencher: &mut test::Bencher) {
+        let mut in_use = vec![];
+        let pool = Arc::new(lockfree_object_pool::LinearObjectPool::new(
+            Vec::new,
+            |buf| buf.clear(),
+        ));
+        bencher.iter(|| {
+            for _ in 0..N {
+                let mut buf = pool.pull_owned();
+                buf.push(Data::default());
+                in_use.push(buf);
+            }
+            for _ in 0..N {
+                in_use.pop().unwrap();
+            }
+        });
+    }
+
+    #[bench]
+    fn bench_arc_pool_scoped(bencher: &mut test::Bencher) {
+        let mut in_use = vec![];
+        let mut pool = arc_buf_pool(u32::MAX as usize, NonZeroUsize::new(4).unwrap());
+        bencher.iter(|| {
+            for _ in 0..N {
+                let mut buf = pool.take_scoped();
+                buf.push(Data::default());
+                in_use.push(buf);
+            }
+            for _ in 0..N {
+                in_use.pop().unwrap();
+            }
+        });
     }
 
     #[bench]
