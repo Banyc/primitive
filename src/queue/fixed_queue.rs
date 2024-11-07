534c74bc8c8499e1b405bf8ea422cb38611a57cc
Banyc <36535895+Banyc@users.noreply.github.com>
2024-11-07 23:20:14 +0800

    fix: `FixedQueue` memory leak

--- src/queue/fixed_queue.rs
+++ src/queue/fixed_queue.rs
@@ -1,26 +1,17 @@
 use core::{marker::PhantomData, mem::MaybeUninit};
 
-use crate::{
-    ops::ring::RingSpace,
-    seq::{Seq, SeqMut},
-    Capacity, Len, LenExt,
-};
+use crate::{ops::ring::RingSpace, seq::SeqMut, Capacity, Len, LenExt};
 
-#[derive(Debug, Clone, Copy)]
-pub struct FixedQueue<S, T> {
+#[derive(Debug)]
+pub struct FixedQueue<S: SeqMut<MaybeUninit<T>>, T> {
     buf: S,
     item: PhantomData<T>,
     prev_head: usize,
     next_tail: usize,
 }
-impl<S, T> FixedQueue<S, T> {
-    pub fn into_buffer(self) -> S {
-        self.buf
-    }
-}
 impl<S, T> FixedQueue<S, T>
 where
-    S: Seq<MaybeUninit<T>>,
+    S: SeqMut<MaybeUninit<T>>,
 {
     #[must_use]
     pub fn new(buf: S) -> Self {
@@ -32,29 +23,6 @@ where
             item: PhantomData,
         }
     }
-}
-impl<S, T> Capacity for FixedQueue<S, T>
-where
-    S: Seq<MaybeUninit<T>>,
-{
-    fn capacity(&self) -> usize {
-        self.buf.as_slice().len().checked_sub(1).unwrap()
-    }
-}
-impl<S, T> Len for FixedQueue<S, T>
-where
-    S: Seq<MaybeUninit<T>>,
-{
-    fn len(&self) -> usize {
-        let capacity = self.capacity();
-        let dist = self.next_tail.ring_sub(self.prev_head, capacity);
-        dist.checked_sub(1).unwrap_or(capacity)
-    }
-}
-impl<S, T> FixedQueue<S, T>
-where
-    S: SeqMut<MaybeUninit<T>>,
-{
     pub fn enqueue(&mut self, item: T) {
         if self.prev_head == self.next_tail {
             panic!("out of buffer space");
@@ -62,13 +30,68 @@ where
         self.buf.as_slice_mut()[self.next_tail] = MaybeUninit::new(item);
         self.next_tail = self.next_tail.ring_add(1, self.capacity());
     }
-    pub fn dequeue(&mut self) -> Option<&T> {
+    pub fn dequeue(&mut self) -> Option<T> {
         if self.is_empty() {
             return None;
         }
-        let head = self.prev_head.ring_add(1, self.capacity());
+        let head = self.head();
         self.prev_head = head;
-        Some(unsafe { self.buf.as_slice().get(head).unwrap().assume_init_ref() })
+        let value = &mut self.buf.as_slice_mut()[head];
+        let value = core::mem::replace(value, MaybeUninit::uninit());
+        Some(unsafe { value.assume_init() })
+    }
+    pub fn iter(&self) -> impl Iterator<Item = &T> + '_ {
+        let head = self.head();
+        (0..self.len()).map(move |i| {
+            let i = head.ring_add(i, self.capacity());
+            let value = &self.buf.as_slice()[i];
+            unsafe { value.assume_init_ref() }
+        })
+    }
+    fn head(&self) -> usize {
+        self.prev_head.ring_add(1, self.capacity())
+    }
+}
+impl<S, T> Capacity for FixedQueue<S, T>
+where
+    S: SeqMut<MaybeUninit<T>>,
+{
+    fn capacity(&self) -> usize {
+        self.buf.as_slice().len().checked_sub(1).unwrap()
+    }
+}
+impl<S, T> Len for FixedQueue<S, T>
+where
+    S: SeqMut<MaybeUninit<T>>,
+{
+    fn len(&self) -> usize {
+        let capacity = self.capacity();
+        let dist = self.next_tail.ring_sub(self.prev_head, capacity);
+        dist.checked_sub(1).unwrap_or(capacity)
+    }
+}
+impl<S, T> Clone for FixedQueue<S, T>
+where
+    S: SeqMut<MaybeUninit<T>> + Clone,
+    T: Clone,
+{
+    fn clone(&self) -> Self {
+        let buf = self.buf.clone();
+        let mut new = Self::new(buf);
+        for item in self.iter() {
+            new.enqueue(item.clone());
+        }
+        new
+    }
+}
+impl<S, T> Drop for FixedQueue<S, T>
+where
+    S: SeqMut<MaybeUninit<T>>,
+{
+    fn drop(&mut self) {
+        while let Some(item) = self.dequeue() {
+            drop(item);
+        }
     }
 }
 
@@ -84,13 +107,14 @@ mod tests {
         assert_eq!(q.len(), 1);
         q.enqueue(2);
         assert_eq!(q.len(), 2);
-        assert_eq!(*q.dequeue().unwrap(), 1);
+        assert_eq!(q.dequeue().unwrap(), 1);
         assert_eq!(q.len(), 1);
         q.enqueue(3);
         assert_eq!(q.len(), 2);
-        assert_eq!(*q.dequeue().unwrap(), 2);
+        assert_eq!(q.clone().iter().copied().collect::<Vec<_>>(), [2, 3]);
+        assert_eq!(q.dequeue().unwrap(), 2);
         assert_eq!(q.len(), 1);
-        assert_eq!(*q.dequeue().unwrap(), 3);
+        assert_eq!(q.dequeue().unwrap(), 3);
         assert!(q.is_empty());
     }
 }
