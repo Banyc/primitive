4e1c67875708904a8b6e2591fbe6cc897cf249e8
Banyc <36535895+Banyc@users.noreply.github.com>
2024-12-21 06:34:52 +0800

    feat: generalize `ExpiringHashMap`

--- src/map/expiring_map.rs
+++ src/map/expiring_map.rs
@@ -1,20 +1,18 @@
 use core::{borrow::Borrow, hash::Hash, time::Duration};
 use std::{collections::HashMap, time::Instant};
 
-use crate::{ops::ord_entry::OrdEntry, queue::ord_queue::OrdQueue};
-
-use super::{
-    hash_map::{HashGetMut, HashRemove},
-    MapInsert,
+use crate::{
+    ops::{len::Len, ord_entry::OrdEntry},
+    queue::ord_queue::OrdQueue,
 };
 
 #[derive(Debug, Clone)]
-pub struct ExpiringHashMap<K, V> {
-    hash_map: HashMap<K, (Instant, V)>,
-    ord_queue: OrdQueue<OrdEntry<Instant, K>>,
+pub struct ExpiringHashMap<K, V, Time, Duration> {
+    hash_map: HashMap<K, (Time, V)>,
+    ord_queue: OrdQueue<OrdEntry<Time, K>>,
     duration: Duration,
 }
-impl<K, V> ExpiringHashMap<K, V> {
+impl<K, V, Time: Ord, Duration> ExpiringHashMap<K, V, Time, Duration> {
     pub fn new(duration: Duration) -> Self {
         Self {
             hash_map: HashMap::new(),
@@ -22,11 +20,11 @@ impl<K, V> ExpiringHashMap<K, V> {
             duration,
         }
     }
-}
-impl<K: Eq + Hash + Clone, V> MapInsert<K, V> for ExpiringHashMap<K, V> {
-    type Out = Option<V>;
-    fn insert(&mut self, key: K, value: V) -> Option<V> {
-        let now = Instant::now();
+    pub fn insert(&mut self, key: K, value: V, now: Time) -> Option<V>
+    where
+        Time: Copy,
+        K: Eq + Hash + Clone,
+    {
         match self.hash_map.insert(key.clone(), (now, value)) {
             Some(prev) => Some(prev.1),
             None => {
@@ -38,14 +36,16 @@ impl<K: Eq + Hash + Clone, V> MapInsert<K, V> for ExpiringHashMap<K, V> {
             }
         }
     }
-}
-impl<K: Eq + Hash + Clone, V> ExpiringHashMap<K, V> {
-    pub fn cleanup(&mut self) {
-        let Some(deadline) = Instant::now().checked_sub(self.duration) else {
+    pub fn cleanup(&mut self, now: Time, mut waste: impl FnMut(K, V, Time))
+    where
+        K: Eq + Hash + Clone,
+        Time: TravelBackInTime<Duration = Duration> + Copy,
+    {
+        let Some(deadline) = now.travel_back_for(&self.duration) else {
             return;
         };
         while let Some(OrdEntry { key: instant, .. }) = self.ord_queue.peek() {
-            if *instant > deadline {
+            if deadline < *instant {
                 return;
             }
 
@@ -57,39 +57,92 @@ impl<K: Eq + Hash + Clone, V> ExpiringHashMap<K, V> {
 
             let real_instant = self.hash_map[&key].0;
 
-            if real_instant > deadline {
+            if deadline < real_instant {
                 self.ord_queue.push(OrdEntry {
                     key: real_instant,
                     value: key,
                 });
             } else {
-                self.hash_map.remove(&key);
+                let (_, value) = self.hash_map.remove(&key).unwrap();
+                waste(key, value, real_instant);
             }
         }
     }
-}
-impl<K: Eq + Hash + Clone, V> HashGetMut<K, V> for ExpiringHashMap<K, V> {
-    fn get_mut<Q>(&mut self, key: &Q) -> Option<&mut V>
+    pub fn contains_key<Q>(&mut self, key: &Q, now: Time, waste: impl FnMut(K, V, Time)) -> bool
     where
-        K: Borrow<Q>,
+        K: Borrow<Q> + Eq + Hash + Clone,
         Q: ?Sized + Eq + core::hash::Hash,
+        Time: TravelBackInTime<Duration = Duration> + Copy,
     {
-        self.cleanup();
+        self.cleanup(now, waste);
+        self.hash_map.contains_key(key)
+    }
+    pub fn get_mut<Q>(
+        &mut self,
+        key: &Q,
+        now: Time,
+        waste: impl FnMut(K, V, Time),
+    ) -> Option<&mut V>
+    where
+        K: Borrow<Q> + Eq + Hash + Clone,
+        Q: ?Sized + Eq + core::hash::Hash,
+        Time: TravelBackInTime<Duration = Duration> + Copy,
+    {
+        self.cleanup(now, waste);
         match self.hash_map.get_mut(key) {
             Some((time, value)) => {
-                *time = Instant::now();
+                *time = now;
                 Some(value)
             }
             None => None,
         }
     }
-}
-impl<K: Eq + Hash + Clone, V> HashRemove<K, V> for ExpiringHashMap<K, V> {
-    fn remove<Q>(&mut self, k: &Q) -> Option<V>
+    pub fn remove<Q>(&mut self, k: &Q) -> Option<(Time, V)>
     where
-        K: Borrow<Q>,
+        K: Borrow<Q> + Eq + Hash + Clone,
         Q: ?Sized + Eq + core::hash::Hash,
     {
-        self.hash_map.remove(k).map(|(_time, value)| value)
+        self.hash_map.remove(k)
     }
 }
+impl<K, V, Time, Duration> Len for ExpiringHashMap<K, V, Time, Duration> {
+    fn len(&self) -> usize {
+        self.hash_map.len()
+    }
+}
+
+pub trait TravelBackInTime: Sized {
+    type Duration;
+    fn travel_back_for(&self, duration: &Self::Duration) -> Option<Self>;
+}
+
+macro_rules! impl_travel_back_for {
+    () => {
+        fn travel_back_for(&self, duration: &Self::Duration) -> Option<Self> {
+            self.checked_sub(*duration)
+        }
+    };
+}
+impl TravelBackInTime for Instant {
+    type Duration = Duration;
+    impl_travel_back_for!();
+}
+macro_rules! same_type_impl_travel_back_in_time {
+    ($ty: ident) => {
+        impl TravelBackInTime for $ty {
+            type Duration = $ty;
+            impl_travel_back_for!();
+        }
+    };
+}
+same_type_impl_travel_back_in_time!(Duration);
+same_type_impl_travel_back_in_time!(u8);
+same_type_impl_travel_back_in_time!(u16);
+same_type_impl_travel_back_in_time!(u32);
+same_type_impl_travel_back_in_time!(u64);
+same_type_impl_travel_back_in_time!(u128);
+same_type_impl_travel_back_in_time!(i8);
+same_type_impl_travel_back_in_time!(i16);
+same_type_impl_travel_back_in_time!(i32);
+same_type_impl_travel_back_in_time!(i64);
+same_type_impl_travel_back_in_time!(i128);
