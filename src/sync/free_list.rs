f060c0b449392d508069c48e67de805a77f5cf56
Banyc <36535895+Banyc@users.noreply.github.com>
2025-05-17 22:41:51 +0800

    docs: `FreeList` reference

--- src/sync/free_list.rs
+++ src/sync/free_list.rs
@@ -2,6 +2,7 @@ use std::mem::{self, MaybeUninit};
 
 use super::{free_u32_list::FreeU32List, sync_unsafe_cell::SyncUnsafeCell};
 
+/// - ref: <https://yeet.cx/blog/lock-free-rust/>
 #[derive(Debug)]
 pub struct FreeList<T, const N: usize> {
     slots: [SyncUnsafeCell<MaybeUninit<T>>; N],
@@ -42,7 +43,7 @@ impl<T, const N: usize> FreeList<T, N> {
     ///
     /// - `index` is from [`Self::try_insert()`]
     /// - once taken after [`Self::try_insert()`], `index` will not be taken anymore unless it has been pulled from [`Self::try_insert()`] later
-    pub unsafe fn take(&self, index: u32) -> T {
+    pub unsafe fn assume_take(&self, index: u32) -> T {
         let index_ = usize::try_from(index).unwrap();
         let slot = unsafe { &mut *self.slots[index_].get() };
         let val = mem::replace(slot, MaybeUninit::uninit());
